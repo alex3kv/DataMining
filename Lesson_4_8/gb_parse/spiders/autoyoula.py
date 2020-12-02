6b915de059521e6759b9e83bcf72c04c83f456cf
Игнат Александр <ignataf@mosreg.ru>
2020-12-02 17:57:36 +0300

    Урок 4, задание из лекции

--- Lesson_4_8/gb_parse/spiders/autoyoula.py
+++ Lesson_4_8/gb_parse/spiders/autoyoula.py
@@ -1,10 +1,38 @@
 import scrapy
+import pymongo
 
 
 class AutoyoulaSpider(scrapy.Spider):
     name = 'autoyoula'
     allowed_domains = ['auto.youla.ru']
     start_urls = ['https://auto.youla.ru/']
+    css_query = {
+        'brands': '.TransportMainFilters_brandsList__2tIkv '
+                  '.ColumnItemList_container__5gTrc .ColumnItemList_column__5gjdt a.blackLink'
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db = pymongo.MongoClient()['parse_11'][self.name]
 
     def parse(self, response):
-        print(1)
+        for link in response.css(self.css_query['brands']):
+            yield response.follow(link.attrib['href'], callback=self.brand_page_parse)
+
+    def brand_page_parse(self, response):
+        for page in response.css('.Paginator_block__2XAPy a.Paginator_button__u1e7D'):
+            yield response.follow(page.attrib['href'], callback=self.brand_page_parse)
+
+        for item_link in response.css('article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu'):
+            yield response.follow(item_link.attrib['href'], callback=self.ads_parse)
+
+    def ads_parse(self, response):
+        title = response.css('.AdvertCard_advertTitle__1S1Ak::text').get()
+        images = [image.attrib['src'] for image in response.css('figure.PhotoGallery_photo__36e_r img')]
+        description = response.css('.AdvertCard_descriptionInner__KnuRi::text').get()
+        self.db.insert_one({
+            'title': title,
+            'images': images,
+            'description': description,
+            'url': response.url
+        })
