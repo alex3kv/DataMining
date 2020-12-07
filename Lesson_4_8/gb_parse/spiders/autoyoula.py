fa9c5c6838a3b2872aab788f117350c834341c9b
Александр Игнат <alex3kv@gmail.com>
2020-12-07 23:21:20 +0300

    Урок 5

--- Lesson_4_8/gb_parse/spiders/autoyoula.py
+++ Lesson_4_8/gb_parse/spiders/autoyoula.py
@@ -1,5 +1,6 @@
+import re
 import scrapy
-import pymongo
+from ..loaders import AutoYoulaLoader
 
 
 class AutoyoulaSpider(scrapy.Spider):
@@ -11,28 +12,44 @@ class AutoyoulaSpider(scrapy.Spider):
                   '.ColumnItemList_container__5gTrc .ColumnItemList_column__5gjdt a.blackLink'
     }
 
+    xpath_query = {
+        'brands': '//div[@class="TransportMainFilters_brandsList__2tIkv"]//a[@class="blackLink"]/@href',
+        'pagination': '//div[contains(@class, "Paginator_block")]//a/@href',
+        'ads': '//article[contains(@class, "SerpSnippet_snippet")]//a[contains(@class, "SerpSnippet_name")]/@href',
+    }
+
+    itm_template = {
+        'title': '//div[@data-target="advert-title"]/text()',
+        'images': '//figure[contains(@class, "PhotoGallery_photo")]//img/@src',
+        'description': '//div[contains(@class, "AdvertCard_descriptionInner")]//text()',
+        'autor': '//script[contains(text(), "window.transitState =")]/text()',
+        'specifications':
+            '//div[contains(@class, "AdvertCard_specs")]/div/div[contains(@class, "AdvertSpecs_row")]',
+    }
+    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.db = pymongo.MongoClient()['parse_11'][self.name]
 
     def parse(self, response):
-        for link in response.css(self.css_query['brands']):
-            yield response.follow(link.attrib['href'], callback=self.brand_page_parse)
+        for link in response.xpath(self.xpath_query['brands']):
+            yield response.follow(link, callback=self.brand_page_parse)
 
     def brand_page_parse(self, response):
-        for page in response.css('.Paginator_block__2XAPy a.Paginator_button__u1e7D'):
-            yield response.follow(page.attrib['href'], callback=self.brand_page_parse)
+        for page in response.xpath(self.xpath_query['pagination']):
+            yield response.follow(page, callback=self.brand_page_parse)
 
-        for item_link in response.css('article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu'):
-            yield response.follow(item_link.attrib['href'], callback=self.ads_parse)
+        for item_link in response.xpath(self.xpath_query['ads']):
+            yield response.follow(item_link, callback=self.ads_parse)
 
     def ads_parse(self, response):
-        title = response.css('.AdvertCard_advertTitle__1S1Ak::text').get()
-        images = [image.attrib['src'] for image in response.css('figure.PhotoGallery_photo__36e_r img')]
-        description = response.css('.AdvertCard_descriptionInner__KnuRi::text').get()
-        self.db.insert_one({
-            'title': title,
-            'images': images,
-            'description': description,
-            'url': response.url
-        })
+        loader = AutoYoulaLoader(response=response)
+        loader.add_value('url', response.url)
+        for name, selector in self.itm_template.items():
+            loader.add_xpath(name, selector)
+        yield loader.load_item()
+
+    def get_specifications(self, response):
+        return {itm.css('.AdvertSpecs_label__2JHnS::text').get(): itm.css(
+            '.AdvertSpecs_data__xK2Qx::text').get() or itm.css('a::text').get() for itm in
+                response.css('.AdvertSpecs_row__ljPcX')}
